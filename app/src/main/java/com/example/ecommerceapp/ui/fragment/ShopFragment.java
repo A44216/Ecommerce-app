eb44694ae0650be013c7459d024f5b78bbbce572
DESKTOP-JTNR84S\Admin <yue2552004@gmail.com>
2026-03-31 00:57:56 +0700

    Create seller UI structure with fragments and layouts

---
+++ app/src/main/java/com/example/ecommerceapp/ui/fragment/ShopFragment.java
@@ -0,0 +1,39 @@
+package com.example.ecommerceapp.ui.fragment;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ecommerceapp.R;
+import com.example.ecommerceapp.ui.viewmodel.ShopViewModel;
+
+public class ShopFragment extends Fragment {
+
+    private ShopViewModel mViewModel;
+
+    public static ShopFragment newInstance() {
+        return new ShopFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_shop, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ShopViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/ecommerceapp/ui/viewmodel/ShopViewModel.java
@@ -0,0 +1,7 @@
+package com.example.ecommerceapp.ui.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class ShopViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
