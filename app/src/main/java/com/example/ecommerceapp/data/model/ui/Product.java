cd0f71512031df4e560e0c8fb60aef1d85e3ad3d
DESKTOP-JTNR84S\Admin <yue2552004@gmail.com>
2026-03-31 18:01:15 +0700

    Xóa image trong model Product

--- app/src/main/java/com/example/ecommerceapp/data/model/ui/Product.java
+++ app/src/main/java/com/example/ecommerceapp/data/model/ui/Product.java
@@ -1,6 +1,7 @@
 package com.example.ecommerceapp.data.model.ui;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public class Product {
 
@@ -9,12 +10,13 @@ public class Product {
     private BigDecimal price;
     private int stock;
     private String description;
-    private String image;
-
+    private List<String> images;
     private String categoryName;
     private String shopName;
 
     // getter
     public String getName() { return name; }
     public BigDecimal getPrice() { return price; }
+    public List<String> getImages() { return images; }
+
 }
